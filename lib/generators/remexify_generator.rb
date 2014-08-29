b77e786c3808885944b96ee4e526859a92563ccf
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-29 08:24:46 +0700

    migration

--- lib/generators/remexify_generator.rb
+++ lib/generators/remexify_generator.rb
@@ -1,13 +1,15 @@
-require "rails/generators/named_base"
+require "rails/generators"
+require "rails/generators/migration"
 
 module Remexify
   module Generators
     class RemexifyGenerator < Rails::Generators::Base
-
-      namespace "remexify"
+      include Rails::Generators::Migration
       source_root File.expand_path("../templates", __FILE__)
 
       desc "Generates a model and database migration for remexify to log error/informations"
+
+      namespace "remexify"
 
       def copy_migration
         migration_template "remexify_log.rb", "db/migration/remexify_log.rb"
