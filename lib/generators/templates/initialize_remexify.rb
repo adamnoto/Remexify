6dc5a37c143805f5464e1b287e11b348d8ec9fe8
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-29 09:40:23 +0700

    remixify

--- lib/generators/templates/initialize_remexify.rb
+++ lib/generators/templates/initialize_remexify.rb
@@ -1,3 +1,1 @@
-Remexify.configure do |config|
-  config.log_table_model = <%= class_name %>
-end
+Remexify.model = <%= class_name %>
--- lib/remexify.rb
+++ lib/remexify.rb
@@ -1,10 +1,5 @@
 require "remexify/version"
-require "gem_config"
 
 module Remexify
-  include GemConfig::Base
-
-  with_configuration do
-    has :log_table_model, classes: Class
-  end
+  mattr_accessor :model
 end
