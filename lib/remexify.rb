5fb38dd402e8f152c2db33d89593cc6622122ccc
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-29 10:54:18 +0700

    remixify

--- lib/remexify.rb
+++ lib/remexify.rb
@@ -1,5 +1,14 @@
 require "remexify/version"
+require "active_support/configurable"
 
 module Remexify
-  mattr_accessor :model
+  include ActiveSupport::Configurable
+
+  class << self
+    def setup
+      config.model = nil
+      yield config
+    end
+  end
+
 end
