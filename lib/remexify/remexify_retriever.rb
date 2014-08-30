258631c36543e323d92243e3c3309d4104bb7781
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-30 16:52:00 +0700

    remexify

--- lib/remexify/remexify_retriever.rb
+++ lib/remexify/remexify_retriever.rb
@@ -3,13 +3,13 @@ module Remexify
     class << self
       def all(options = {})
         if options[:order]
-          return config.model.order(options[:order]).all.to_a
+          return Remexify.config.model.order(options[:order]).all.to_a
         end
-        return config.model.all.to_a
+        return Remexify.config.model.all.to_a
       end
 
       def all_today(options = {})
-        ar_statement = config.model.where(created_at: Time.now.beginning_of_day)
+        ar_statement = Remexify.config.model.where(created_at: Time.now.beginning_of_day)
         if options[:order]
           return ar_statement.order(options[:order]).all.to_a
         end
@@ -20,7 +20,7 @@ module Remexify
 
   class << self
     def delete_all_logs
-      config.model.delete_all
+      Remexify.config.model.delete_all
     end
   end
 end
