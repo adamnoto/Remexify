67bc290a961b8603ebac90f52d22f7c15f786571
Adam Pahlevi <adam.pahlevi@gmail.com>
2014-08-31 07:43:24 +0700

    adding level as option key

--- lib/remexify/remexify_retriever.rb
+++ lib/remexify/remexify_retriever.rb
@@ -1,23 +1,59 @@
+require "singleton"
+
 module Remexify
   module Retrieve
     class << self
       def all(options = {})
-        if options[:order]
-          return Remexify.config.model.order(options[:order]).all.to_a
-        end
-        return Remexify.config.model.all.to_a
+        logs = Remexify.config.model.all
+        Remexify::Retrieve::Filter.instance.order logs, options
+        Remexify::Retrieve::Filter.instance.level logs, options
+
+        logs.to_a
       end
 
       def today(options = {})
-        ar_statement = Remexify.config.model.where(created_at: Time.now.beginning_of_day..Time.now.end_of_day)
-        if options[:order]
-          return ar_statement.order(options[:order]).all.to_a
-        end
-        return ar_statement.all.to_a
+        logs = Remexify.config.model.where(created_at: Time.now.beginning_of_day..Time.now.end_of_day).all
+        Remexify::Retrieve::Filter.instance.order logs, options
+        Remexify::Retrieve::Filter.instance.level logs, options
+
+        logs.to_a
       end
 
       def where_fingerprint_is(fingerprint)
         Remexify.config.model.where(md5: fingerprint).first
+      end
+    end
+  end
+
+  class Remexify::Retrieve::Filter
+    include Singleton
+
+    def order(obj, options)
+      if options[:order]
+        obj.order(options[:order])
+      end
+    end
+
+    def level(obj, options)
+      lvl_option = options[:level]
+      if lvl_option
+        lvl = lvl.gsub(/[^0-9]/, "")
+        raise "Level must all be a number" if (lvl =~ /^[0-9]+$/).nil?
+
+        if lvl =~ />=/
+          obj.where("level >= ?", lvl)
+        elsif lvl =~ /<=/
+          obj.where("level <= ?", lvl)
+        elsif lvl =~ />/
+          obj.where("level > ?", lvl)
+        elsif lvl =~ /</
+          obj.where("level < ?", lvl)
+        elsif lvl =~ /=/
+          obj.where("level = ?", lvl)
+        else
+          raise "Unknown operator for level"
+        end
+
       end
     end
   end
